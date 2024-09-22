f41b845aef1b1ef91c4e692703f313a03840022b
Blake <kibokpark9268@naver.com>
2024-09-22 22:38:48 +0900

    unore pipeline - dag, function

---
+++ dags/unore.py
@@ -0,0 +1,265 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+from airflow.providers.slack.operators.slack import SlackAPIPostOperator
+from airflow.models.baseoperator import chain
+from airflow.models import Variable
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+import ast
+
+import sys
+sys.path.append('/opt/airflow/jobs')
+from unore_comtradeapi import del_old_csvfile, unore_each_month_chk_need_updates, format_dict, chk_csvfile
+
+
+# 오류시 메시지를 보낼 함수 정의
+def slack_failure_callback(context):
+    slack_msg = f"""
+    :red_circle: DAG Failed
+    *Task*: {context.get('task_instance').task_id}
+    *Dag*: {context.get('task_instance').dag_id}
+    *Execution Time*: {context.get('execution_date')}
+    *Log URL*: {str(context.get('task_instance').log_url).replace(':8080',':8082')}
+    """
+    
+    slack_alert = SlackAPIPostOperator(
+            slack_conn_id="slack_pkb",
+            task_id='send_error_with_slack',
+            channel='#unore_alarm',  # 전송할 Slack 채널
+            dag=dag,
+            blocks=[
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": (
+                            slack_msg
+                        ),
+                    },
+                }
+            ],
+            text="Airflow_unore_error",  # 필수 fallback 메시지
+        )
+    return slack_alert.execute(context=context)
+
+# DAG
+dag = DAG("unore_pipeline",
+          default_args={
+            "owner": "airflow",
+            "depends_on_past": False, # 과거 실행에 의존
+            "start_date": datetime(2024, 9, 20),
+            "retries": 2,             # retry 횟수
+            "retry_delay": timedelta(minutes=10), # retry주기
+            'on_failure_callback': slack_failure_callback,
+            },
+            schedule_interval = "0 5 * * *",
+            catchup=False, 
+            tags=['PKB','unore'])
+
+
+# Task0 : target_date 설정 (catchup 등으로 실행하는 부분 고려)
+def set_basemonth_updateall_targetdate(**kwargs):
+    # Check and print logical date
+    print(f"Logical_date : {kwargs['logical_date']}") # logical_date : 전일 0시 ~ 금일 0시
+
+    # Calculate basemonth(API's column : Period)
+    months = []
+    for i in range(1, 14): # 14 : 1년
+        temp_month = kwargs['logical_date'] - relativedelta(months=i)
+        months.append(temp_month.strftime('%Y%m'))
+
+    # Set airflow variable : Update all data at 1st week of each month
+    if kwargs['logical_date'].strftime('%d') in ['01', '02', '03', '04', '05', '06', '07','22']:
+        Variable.set('unore_update_all', 'True')
+    else:
+        Variable.set('unore_update_all', 'False')
+
+    Variable.set('unore_targetdate', kwargs['logical_date'].strftime('%Y%m%d'))
+    
+    # Print result for logs of airflow or spark
+    print(f"months : {months}")
+    print(f"unore_update_all : {Variable.get('unore_update_all')}")
+
+    return months
+
+down_month_calculate = PythonOperator(
+    task_id='calculate_month_date',
+    python_callable=set_basemonth_updateall_targetdate,
+    provide_context=True,
+    dag=dag,
+)
+
+# Task1 : 업데이트 대상 확인
+index_name ='ore_index1'
+
+chk_needs_updates = PythonOperator(
+    task_id='chk_needs_updates',
+    python_callable=unore_each_month_chk_need_updates,
+    op_kwargs={"list_month": "{{task_instance.xcom_pull(task_ids='calculate_month_date')}}",
+               "index_name":f"{index_name}"},
+    provide_context=True,
+    dag=dag,
+)
+
+# Task2: 전체 업데이트 여부(update_all)와 업데이트 대상 월 알림
+update_all = Variable.get('unore_update_all')
+txt_needs_update = Variable.get('unore_needs_update')
+
+send_slack_update_status = SlackAPIPostOperator(
+    slack_conn_id="slack_pkb",
+    task_id='slack_chk_update_target',
+    channel='#unore_alarm',  # 전송할 Slack 채널
+    dag=dag,
+    blocks=[
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": (
+                    "*[UN ORE 업데이트 대상 확인 결과]*\n\n"
+                    f"* *update all* : {update_all}\n\n"
+                    f"* *List month* \n\n{txt_needs_update}"
+                ),
+            },
+        }
+    ],
+    text="unore_chk_update_target",  # 필수 fallback 메시지
+)
+
+
+# Task3 : 업데이트 시작
+spark_unore = SparkSubmitOperator(
+        task_id='spark_unore',
+        application="jobs/unore_comtradeapi.py", # 절대경로라면 /opt/airflow/jobs/main.py
+        application_args=["--list_month", "{{ task_instance.xcom_pull(task_ids='calculate_month_date') }}",
+                          "--update_all", f"{update_all}",
+                          "--index_name", f"{index_name}",
+                          "--function", "unore_each_month_update_to_es_with_spark",
+                          "--dict_reportercode_need_update", "{{ task_instance.xcom_pull(task_ids='chk_needs_updates') }}"],
+        name="unore_main",
+        conf={
+            'spark.master': 'spark://spark-master:7077',  # master 설정
+            'spark.dynamicAllocation.enabled': 'true',
+            'spark.dynamicAllocation.executorIdleTimeout': '2m',
+            'spark.dynamicAllocation.minExecutors': '1',
+            'spark.dynamicAllocation.maxExecutors': '3',
+            'spark.dynamicAllocation.initialExecutors': '1',
+            'spark.memory.offHeap.enabled': 'true',
+            'spark.memory.offHeap.size': '2G',
+            'spark.shuffle.service.enabled': 'true',
+            'spark.executor.memory': '2G',
+            'spark.driver.memory': '2G',
+            'spark.driver.maxResultSize': '0',
+        },
+        conn_id="spark-conn", # 필수값. UI에서 conenctivity 설정해둔 기준
+        jars="/opt/bitnami/spark/resources/elasticsearch-spark-30_2.12-8.4.3.jar",
+        executor_cores=1,
+        num_executors=2,
+        verbose=1,
+        dag=dag
+    )
+
+
+# spark_unore = SparkSubmitOperator(
+#         task_id='spark_unore',
+#         application="jobs/unore_comtradeapi.py", # 절대경로라면 /opt/airflow/jobs/main.py
+#         application_args=["--list_month", "{{ task_instance.xcom_pull(task_ids='calculate_month_date') }}",
+#                           "--update_all", f"{update_all}"],
+#         name="unore_main",
+#         conf={
+#             'spark.master': 'spark://spark-master:7077',  # master 설정
+#             'spark.dynamicAllocation.enabled': 'true',
+#             'spark.dynamicAllocation.executorIdleTimeout': '2m',
+#             'spark.dynamicAllocation.minExecutors': '1',
+#             'spark.dynamicAllocation.maxExecutors': '3',
+#             'spark.dynamicAllocation.initialExecutors': '1',
+#             'spark.memory.offHeap.enabled': 'true',
+#             'spark.memory.offHeap.size': '2G',
+#             'spark.shuffle.service.enabled': 'true',
+#             'spark.executor.memory': '2G',
+#             'spark.driver.memory': '2G',
+#             'spark.driver.maxResultSize': '0',
+#         },
+#         conn_id="spark-conn", # 필수값. UI에서 conenctivity 설정해둔 기준
+#         jars="/opt/bitnami/spark/resources/elasticsearch-spark-30_2.12-8.4.3.jar",
+#         executor_cores=1,
+#         num_executors=2,
+#         verbose=1,
+#         dag=dag
+#     )
+
+# Task4 : 업데이트 내역
+chk_updated_files = PythonOperator(
+    task_id='chk_updeated_csv',
+    python_callable=chk_csvfile,
+    provide_context=True,
+    dag=dag,
+)
+
+send_slack_updated_df = SlackAPIPostOperator(
+    slack_conn_id="slack_pkb",
+    task_id='slack_ore_updated_df',
+    channel='#unore_alarm',  # 전송할 Slack 채널
+    dag=dag,
+    blocks=[
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": (
+                    "*[CSV Save내역]*\n\n{{ task_instance.xcom_pull(task_ids='chk_updeated_csv') }}"
+                ),
+            },
+        }
+    ],
+    text="unore_result_updated_df",  # 필수 fallback 메시지
+)
+
+# Task final
+# 오래된 파일 삭제
+def set_delete_date(**kwargs):
+    print(f"Logical_date : {kwargs['logical_date']}") # logical_date : 전일 0시 ~ 금일 0시
+    target_date_delete = (kwargs['logical_date'] - timedelta(days=364)).strftime('%Y-%m-%d')
+    print(f'Calculated target_date_delete : {target_date_delete}')
+    return target_date_delete
+
+delete_date_calculate = PythonOperator(
+    task_id='calculate_delete_date',
+    python_callable=set_delete_date,
+    provide_context=True,
+    dag=dag,
+)
+
+del_old_csv_file = PythonOperator(
+    task_id='del_old_csv_file',
+    python_callable=del_old_csvfile,
+    op_kwargs={"target_date_delete": "{{task_instance.xcom_pull(task_ids='calculate_delete_date')}}"},
+    provide_context=True,
+    dag=dag,
+)
+
+# Task3 : 결과 전송 with Slack (위 Task에서 리턴받은 결과를 출력)
+send_slack_delete_history = SlackAPIPostOperator(
+    slack_conn_id="slack_pkb",
+    task_id='slack_old_csv_delete',
+    channel='#unore_alarm',  # 전송할 Slack 채널
+    dag=dag,
+    blocks=[
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": (
+                    "*[Old CSV 삭제내역]*\n\n{{ task_instance.xcom_pull(task_ids='del_old_csv_file') }}"
+                ),
+            },
+        }
+    ],
+    text="unore_result_old_csv_delete",  # 필수 fallback 메시지
+)
+
+# Flow
+
+chain(down_month_calculate, chk_needs_updates, send_slack_update_status, spark_unore, chk_updated_files, send_slack_updated_df)
+chain(delete_date_calculate, del_old_csv_file, send_slack_delete_history)
---
+++ jobs/unore_comtradeapi-all_func_in_onefile.py
@@ -0,0 +1,326 @@
+import pandas as pd
+from pandas import DataFrame
+from datetime import datetime, timedelta
+import comtradeapicall
+import argparse
+import ast
+from es import Es, Es_customid
+from elasticsearch.exceptions import NotFoundError
+from airflow.models import Variable
+from pyspark.sql import SparkSession
+from base import df_with_meta
+import os
+import numpy as np
+import glob
+
+# Setting madatory
+subscription_key = Variable.get('unore_subscription_key')
+csv_path = '/opt/bitnami/spark/data/unore'
+
+hscode = {'Nickel':'282540,283324',
+          'Cobalt':'282200,283329',
+          'Lithium':'282520,283691',
+          'Manganese':'850610'}
+
+def chk_updated_data_month(subscription_key:str, period:str, reporterCode=None)->DataFrame:
+
+    # Call API to check latest data
+    mydf = comtradeapicall.getFinalDataAvailability(subscription_key, 
+                                                    typeCode='C', #  Goods (C) or Services (S)
+                                                    freqCode='M', # Annual (A) or Monthly (M)
+                                                    clCode='HS',  # HS, SITC
+                                                    period=period, # Annual (2023) or Monthly (202306)
+                                                    reporterCode=reporterCode) # None : All
+    
+    if len(mydf) == 0:
+        print('Result of chk_updated_data_month')
+        print('**UN dont have this data please try later**')
+        return mydf
+    else:
+        # Change lastReleased from str to datetime
+        mydf['lastReleased'] = pd.to_datetime(mydf['lastReleased'], format='mixed')
+
+        # change reportercode from int to str
+        mydf['reporterCode'] = mydf['reporterCode'].astype('str') 
+
+        # Print result for check with Airflow or Spark
+        print('Result of chk_updated_data_month')
+        print(mydf.to_string(index=False))
+
+        return mydf
+
+def chk_last_released_reportercode(mydf:DataFrame, ndays_from_today:int=7, update_all:bool=False)->DataFrame:
+    # Calculate date
+    base_date = datetime.now() - timedelta(days=ndays_from_today)
+
+    # Filter updated Country(reportercode) within ndays from today OR all
+    if update_all:
+        filtered = mydf[['reporterCode','reporterDesc','lastReleased']]
+    else:
+        filtered = mydf[mydf['lastReleased'] >= base_date][['reporterCode','reporterDesc','lastReleased']] # filter
+
+    # change data type
+    filtered['reporterCode'] = filtered['reporterCode'].astype('str') # change reportercode from int to str
+    filtered['lastReleased'] = filtered['lastReleased'].dt.strftime('%Y-%m-%d') # convert datetime format
+
+    # Print result for check with Airflow or Spark
+    print('Result of chk_last_released_reportercode')
+    print(filtered.to_string(index=False))
+
+    return filtered
+
+def call_data_with_hscode_month(subscription_key:str, period:str, hscode:dict, reporterCode=None)->DataFrame:
+    # Made HS code string for faster result of API
+    str_hscode = ','.join(hscode.values())
+
+    # Call API and receive data
+    mydf = comtradeapicall.getFinalData(subscription_key, typeCode='C', freqCode='M', clCode='HS', 
+                                        period=period, reporterCode=reporterCode, cmdCode=str_hscode, # from argument
+                                        flowCode='M,X', # M : Import / X : Export,
+                                        partnerCode=None, partner2Code=None,customsCode=None, motCode=None, 
+                                        maxRecords='250000', format_output='JSON', aggregateBy=None, breakdownMode='classic', 
+                                        countOnly=None, includeDesc=True)
+    
+    # Filter specific columns
+    mydf = mydf[['refYear', 'refMonth', 'refPeriodId', 'period',
+       'reporterCode', 'reporterISO', 'reporterDesc', 'flowCode', 'flowDesc',
+       'partnerCode', 'partnerISO', 'partnerDesc', 'classificationCode',
+       'cmdCode', 'cmdDesc', 'customsCode', 'customsDesc',
+       'qtyUnitCode', 'qtyUnitAbbr', 'qty', 'altQtyUnitCode', 'altQtyUnitAbbr', 'altQty',
+       'netWgt', 'grossWgt', 'cifvalue', 'fobvalue', 'primaryValue',]]
+    
+    # change important columns' data type
+    mydf['reporterCode'] = mydf['reporterCode'].astype('str') 
+    mydf['refPeriodId'] = mydf['refPeriodId'].astype('str')
+    mydf['refPeriodId'] = pd.to_datetime(mydf['refPeriodId'], format='%Y%m%d', errors='coerce')   
+    mydf['refPeriodId'] = mydf['refPeriodId'].dt.strftime('%Y-%m-%d')
+
+    # Add product name column
+    new_hscode = {}
+    for key, values in hscode.items():
+        for code in values.split(','):
+            new_hscode[code] = key
+    mydf['product_name'] = mydf['cmdCode'].copy()
+    mydf['product_name'] = mydf['product_name'].replace(new_hscode)
+
+    # Add PricePerKG column
+    mydf.replace(0, np.nan, inplace=True)
+    mydf['baseqty'] = mydf[['qty', 'altQty', 'netWgt', 'grossWgt']].min(axis=1)
+    mydf['baseqty'] = mydf['baseqty'].dropna()
+    mydf['PricePerPKG'] = mydf['primaryValue']/mydf['baseqty']
+
+    # Change column types from object to string
+    for columns in mydf.columns:
+        if mydf[columns].dtype == 'object':
+            mydf[columns] =  mydf[columns].astype(str)
+        elif mydf[columns].dtype == 'float64':
+            mydf[columns] = mydf[columns].fillna(0) # This must be later than PricePerKG task
+
+    # Made custom_id for ES
+    mydf['custom_id'] = mydf['period'] + mydf['cmdCode'] + mydf['reporterISO'] + mydf['partnerISO'] + mydf['flowCode']
+    
+    # Print result for check with Airflow or Spark
+    print('Result of call_data_with_hscode_month')
+    print(mydf.to_string(index=False))
+
+    return mydf
+
+def unore_chk_need_updates(base_month:str, index_name:str):
+
+    ## Check with API
+    mydf = chk_updated_data_month(subscription_key, base_month)
+    if len(mydf) == 0:
+        return dict() # return empty dict for not doing further tasks
+    df_reporter_code_latest = chk_last_released_reportercode(mydf, 30)
+    list_reporterCode_latest = list(df_reporter_code_latest['reporterCode']) # ES multi search에 사용
+    number_newest_api = len(df_reporter_code_latest) # 저장된 데이터 작업여부 판단에 사용
+
+    ### Save to csv
+    mydf.to_csv(f"{csv_path}/{','.join(hscode.values())}_{base_month}_1availablity_all_{datetime.now().strftime('%Y%m%d')}.csv", index=False)
+    df_reporter_code_latest.to_csv(f"{csv_path}/{','.join(hscode.values())}_{base_month}_2availablity_latest_{datetime.now().strftime('%Y%m%d')}.csv", index=False)
+
+    ## Check with ES
+    es =Es_customid("http://es:9200")
+    try:
+        query = {
+            "bool": {
+                "must": [
+                    {"terms": {"reporterCode": list_reporterCode_latest}},  # reporterCode 목록에 있는 값
+                    {"term": {"period": base_month}}
+                ]
+            }
+        }
+        response = es.send_query_to_index(index_name=index_name, query=query, row_limit=10000)
+        dict_es_oldest_date = es.chk_oldest_timestamp_of_es_with_field(response)
+        print('Result of chk_oldest_timestamp_of_es_with_field')
+        print(dict_es_oldest_date)
+    except NotFoundError as e:
+        dict_es_oldest_date = {}
+        print('No index to search, should make new index')
+
+    ### Save to csv
+    esdf = pd.DataFrame.from_dict(dict_es_oldest_date, orient='index', columns=['timestamp'])
+    esdf.to_csv(f"{csv_path}/{','.join(hscode.values())}_{base_month}_3es_latest_{datetime.now().strftime('%Y%m%d')}.csv")
+
+    ## Check reportercode which is not updated
+    dict_reportercode_need_update = {}
+    
+    if number_newest_api != 0:
+        for reporterCode in df_reporter_code_latest['reporterCode']:
+            date_of_api = df_reporter_code_latest[df_reporter_code_latest['reporterCode']==reporterCode]['lastReleased'].item()
+            date_of_api = datetime.strptime(date_of_api, "%Y-%m-%d")
+            if len(dict_es_oldest_date) == 0: # ES가 아예 Empty인 경우
+                dict_reportercode_need_update[reporterCode] = df_reporter_code_latest[df_reporter_code_latest['reporterCode']==reporterCode]['reporterDesc'].item() # reporterCode    
+            elif reporterCode not in dict_es_oldest_date.keys(): # ES에 일부 값은 있지만 해당 reportercode 없는 경우
+                dict_reportercode_need_update[reporterCode] =  df_reporter_code_latest[df_reporter_code_latest['reporterCode']==reporterCode]['reporterDesc'].item() # reporterCode    
+            elif dict_es_oldest_date[reporterCode] < date_of_api:  # ES에 reportercode 있지만 업데이트일자가 최신이 아니면
+                dict_reportercode_need_update[reporterCode] =  df_reporter_code_latest[df_reporter_code_latest['reporterCode']==reporterCode]['reporterDesc'].item() # reporterCode    
+    
+    print('List of reportercode needs_update')
+    print(dict_reportercode_need_update)
+
+    return dict_reportercode_need_update
+
+def unore_update_to_es_with_spark(base_month:str, index_name:str, dict_reportercode_need_update:dict, update_all:bool=False):
+    # Prepare Spark & ES
+    spark = (SparkSession
+        .builder
+        .master("local")
+        .appName("convert_dataframe_and save_to_es")
+        .config("spark.driver.extraClassPath", "/opt/bitnami/spark/resources/elasticsearch-spark-30_2.12-8.4.3.jar") # 원본
+        .config("spark.jars", "/opt/bitnami/spark/resources/elasticsearch-spark-30_2.12-8.4.3.jar")
+        # 옵션추가 시작
+        .config("spark.executor.memory","3G")
+        .config("spark.driver.memory","3G")
+        .config("num-executor",2)
+        .config("executor-cores",2)
+        # 옵션추가 끝
+        .getOrCreate())
+    es =Es_customid("http://es:9200")
+    
+    # Call API for data needs update
+    number_need_update = len(dict_reportercode_need_update)
+    str_reporterCode_latest = ','.join(dict_reportercode_need_update.keys()) # 저장할 데이터 API Call에 사용
+    str_spark_df = ''
+    return_df = True
+
+    # Convert pandas Dataframe to Spark Dataframe
+    if len(chk_updated_data_month(subscription_key, base_month)) == 0:
+        # Chk API's data first, and do nothing if API's data is empty
+        str_spark_df = 'UN API dont have data, please try later'
+        return_df = False
+    elif update_all: 
+        # 무조건 업데이트로 설정시 전체업데이트
+        ## Receive ALL date from API (type:DataFrame)
+        df = call_data_with_hscode_month(subscription_key, base_month, hscode)
+        ## Convert pandas Dataframe to Spark Dataframe
+        spark_df = spark.createDataFrame(df)
+        spark_df = df_with_meta(spark_df, datetime.now().strftime('%Y-%m-%d'))
+        spark_df.show(50)
+        str_spark_df = spark_df._jdf.showString(20, 50, False)
+        ## Save Spark Dataframe to ES
+        es.write_df(df=spark_df, es_resource=index_name,  es_write_operation="upsert", custom_id="custom_id")
+        return_df = True
+    elif number_need_update != 0:
+        # 업데이트할 대상이 있는 경우 (1개 이상)
+        ## Receive date from API (type:DataFrame)
+        df = call_data_with_hscode_month(subscription_key, base_month, hscode, str_reporterCode_latest)
+        ## Convert pandas Dataframe to Spark Dataframe
+        spark_df = spark.createDataFrame(df)
+        spark_df = df_with_meta(spark_df, datetime.now().strftime('%Y-%m-%d'))
+        spark_df.show(50, False)
+        str_spark_df = spark_df._jdf.showString(20, 50, False)
+        ## Save Spark Dataframe to ES
+        es.write_df(df=spark_df, es_resource=index_name,  es_write_operation="upsert", custom_id="custom_id")
+        return_df = True
+    else:
+        # 업데이트할 대상이 없는 경우(0개)
+        str_spark_df = 'Skipped update because all of data is latest'
+        return_df = False
+
+    # Save to airflow variable, csv file for further check / Print Spark Dataframe
+    Variable.set("unore_df", str_spark_df)
+    print('***************************************************************************')
+    print(str_spark_df)
+    print('***************************************************************************')
+
+    # Return Spark Dataframe if exists
+    if return_df:
+        df.to_csv(f"{csv_path}/{','.join(hscode.values())}_{base_month}_4actual_{datetime.now().strftime('%Y%m%d')}.csv", index=False)
+        return spark_df
+    else:
+        return 'Nothing updated. UN API have no data yet, or saved data is latest'
+
+def unore_all_main(list_month:list, index_name:str, update_all:bool=False)->dict:
+
+    for base_month in list_month:
+        print(f"Current base month in unore_main loop : {base_month}")
+        # Check update info(API) and exist info(ES) and filter which(reportercode) require update
+        dict_reportercode_need_update = unore_chk_need_updates(base_month, index_name)
+        
+        # If required : update / convert to spark / save to ES
+        spark_df = unore_update_to_es_with_spark(base_month, index_name, dict_reportercode_need_update, update_all)
+    
+    return spark_df
+
+def del_old_csvfile(target_date_delete:str)->str:
+
+    def format_dict(dictionary):
+        formatted_str = ''
+        for each_key in dictionary.keys():
+            if len(dictionary[each_key]) == 0:
+                formatted_str = formatted_str + f"*{each_key}* \n• 삭제건 없음\n\n"
+            else:
+                formatted_str = formatted_str + f"*{each_key}* \n{chr(10).join([f'• {item}' for item in dictionary[each_key]])}\n\n"
+        return formatted_str
+
+    print(f'Recevied target_date_delete : {target_date_delete}')
+
+    date_to_delete = datetime.strptime(target_date_delete, '%Y-%m-%d') # convert to datetime
+    file_pattern = f"{csv_path}/{','.join(hscode.values())}*{date_to_delete.strftime('%Y%m%d')}.csv"
+    print(file_pattern)
+    matching_files = glob.glob(file_pattern)
+
+    # Delete each matching file
+    dict_deleted = {'Deleted':[]}
+    for file_path in matching_files:
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+            dict_deleted['Deleted'].append(file_path)
+
+    return format_dict(dict_deleted)
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--list_month', required=True,)
+    parser.add_argument('--update_all', required=True,)
+    args = parser.parse_args()
+
+    # Convert args (from str to others)
+    if args.update_all == 'True':
+        args.update_all = True
+    elif args.update_all == 'False':
+        args.update_all = False
+    else: # 기본값은 False (API호출 최소화 목적)
+        args.update_all = False
+    print(f"args.update_all (type/data): {type(args.update_all)}/{args.update_all}")
+
+    print(f"list_month before (type/data): {type(args.list_month)}/{args.list_month}")
+    list_month = ast.literal_eval(args.list_month)
+    print(f"list_month after (type/data): {type(list_month)}/{list_month}")
+
+
+    # Check Latest data
+    index_name ='ore_index1'
+
+
+    # Main : update datas
+    #list_month = ['202412'] # for test
+    unore_all_main(list_month=list_month, index_name=index_name, update_all=args.update_all)
+
+
+    # Delete test
+    #del_old_csvfile('2024-09-22')
+    
+
